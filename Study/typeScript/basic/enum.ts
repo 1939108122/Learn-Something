eed1f33b371ce04881aeff8fc9c01d768bc4d993
huwenkai@kanzhun.com <1939108122@qq.com>
2022-07-19 14:30:56 +0800

    选区学习

---
+++ Study/typeScript/basic/enum.ts
@@ -0,0 +1,6 @@
+enum Color {
+    green = 2,
+    blue = 4,
+    red
+}
+console.log(Color['green']);
---
+++ Study/typeScript/basic/nulll&undefined.ts
@@ -0,0 +1,5 @@
+let n :null = null;
+let u :undefined = undefined;
+let a: number = 1;
+// a = n;
+// a = null;
