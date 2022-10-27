1803d2325a419b8463dc656cf5e207c40f9acc0d
huwenkai@kanzhun.com <1939108122@qq.com>
2022-10-27 23:18:34 +0800

    feat

---
+++ Study/Vue2/Vuex/vuex-demo/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <Add></Add>
+    <p>---------------------</p>
+    <Sub></Sub>
+  </div>
+</template>
+
+<script>
+
+import Add from './components/add.vue';
+import Sub from './components/sub.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Add,
+    Sub
+  }
+}
+</script>
+
+<style>
