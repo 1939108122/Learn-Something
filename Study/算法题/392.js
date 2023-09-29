f524fe90dc873fbbfbdcc19a5e29f19d38e9082c
huwenkai <1939108122@qq.com>
2023-09-29 20:20:32 +0800

    判断子序列

---
+++ Study/算法题/392.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function(s, t) {
+    let slow = 0;
+    for(let i = 0; i < t.length; i++) {
+        if(s[slow] === t[i]) {
+            slow++;
+        }
+    }
+
+    return slow === s.length;
+    
+};
